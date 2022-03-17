cdb3df8b17f4d1dbb919d363a6374842636679c0
osama-abdallah <osama.i.abdallah95@gmail.com>
2022-03-17 09:13:14 +0300

    reDone3

--- javascript/code-challenges/data-structures/linked-lists/linked-list-kth/ll.js
+++ javascript/code-challenges/data-structures/linked-lists/linked-list-kth/ll.js
@@ -46,6 +46,91 @@ class LinkedList {
     }
     currentNode.next = node;
   }
+
+  insertBefore(value, newVal) {
+    const node = new Node(newVal);
+    let currentNode = this.head;
+
+    while (currentNode !== null) {
+      if (this.head.value === value) {
+        this.insert(newVal);
+        break;
+      }
+      if (currentNode.next.value === value) {
+        node.next = currentNode.next;
+        currentNode.next = node;
+        break;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+    if (!currentNode) {
+      return new Error('Value not found in list.');
+    }
+  }
+
+  insertAfter(value, newVal) {
+    const node = new Node(newVal);
+    if (!this.head) {
+      const error = new Error('Can not search an empty linked list.');
+      return error;
+    }
+    let currentNode = this.head;
+
+    while (currentNode !== null) {
+      if (currentNode.next === null) {
+        this.append(newVal);
+        break;
+      }
+      if (currentNode.value === value) {
+        node.next = currentNode.next;
+        currentNode.next = node;
+        break;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+    if (!currentNode) {
+      return new Error('Value not found in list.');
+    }
+  }
+  insert(value) {
+    const node = new Node(value);
+    node.next = this.head;
+    this.head = node;
+    return;
+  }
+  includes(testValue) {
+    if(!this.head) {
+      return false;
+    }
+    let currentNode = this.head;
+    while(currentNode !== null) {
+      if (currentNode.value === testValue) {
+        return true;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+    return false;
+  }
+  toString() {
+    let arrayString = '';
+    let currentNode = this.head;
+
+    if (!this.head) {
+      return `NULL`;
+    } else {
+      arrayString = `{${this.head.value}} -> `;
+    }
+    while(currentNode.next !== null) {
+      currentNode = currentNode.next;
+      arrayString += `{${currentNode.value}} -> `;
+    }
+    arrayString += `NULL`;
+
+    return arrayString;
+  }
 }
 console.log(Node);
 
