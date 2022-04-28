17a71ea5ac9431e35ba415cb50ffb6e148e06598
osama-abdallah <osama.i.abdallah95@gmail.com>
2022-04-28 21:55:32 +0300

    complete

---
+++ javascript/code-challenges/Merge-Sort/BLOG.md
@@ -0,0 +1,12 @@
+# Challenge Summary
+
+Merges two arrays. Accept two arrays, and it needs to sort the two arrays correctly starting from the smallest element. With that you merge sort ready.
+
+## Whiteboard Process
+
+![image](./assets/Merge-Sort.png)
+
+## Approach & Efficiency
+
+Big O Time : O(n*log n)
+Big O Space : O(1)
---
+++ javascript/code-challenges/Merge-Sort/index.js
@@ -0,0 +1,49 @@
+'use strict';
+function mergeSort(arr) {
+  let n = arr.length;
+  if (n > 1) {
+    let mid = n / 2;
+    let left = arr.slice(0, mid);
+    let right = arr.slice(mid);
+    mergeSort(left);
+    mergeSort(right);
+    merge(left, right, arr);
+  }
+  return arr;
+}
+
+
+function merge(left, right, arr) {
+  let i = 0;
+  let j = 0;
+  let k = 0;
+  while (i < left.length && j < right.length) {
+    if (left[i] <= right[j]) {
+      arr[k] = left[i];
+      i++;
+    }
+    else {
+      arr[k] = right[j];
+      j++;
+    }
+    k++;
+  }
+  while (i < left.length) {
+    arr[k] = left[i];
+    i++;
+    k++;
+  }
+
+  while (j < right.length) {
+    arr[k] = right[j];
+    j++;
+    k++;
+  }
+}
+
+
+
+
+console.log( mergeSort([8,4,23,42,16,15]));
+
+module.exports = mergeSort;
